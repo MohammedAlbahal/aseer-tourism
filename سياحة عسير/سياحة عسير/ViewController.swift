8b6bfc8db3afa72430c8cb825c75d1d121ef3485
mohammedali111 <91420952+mohammedali111@users.noreply.github.com>
2022-02-08 22:40:35 +0300

    add first commit the of app

---
+++ سياحة عسير/سياحة عسير/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  سياحة عسير
+//
+//  Created by saja Al-qhtani on 07/07/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
